e7455e3a9c53c2ef53a6dca3eccec6908305137b
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 14:51:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,6 +1,6 @@
 def help
-  puts "I accept the following commands:
-- help : displays this help message
+  puts "I accept the following commands:"
+  puts - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
 - exit : exits this program"

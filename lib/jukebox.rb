ace3e2c27bac29e427f741bb49559dfb554468b4
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 14:54:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,20 +6,26 @@ def help
   puts "- exit : exits this program"
 end
 
+
 def list(songs)
-  # Displays each song with a 
+  
+  # Displays each song with a track number
   songs.each_with_index { |song, idx| puts "#{idx + 1}. #{song}" }
 end
 
+
 def play(songs)
+  
+  # Prompts user for a song name or number, and get user's input
   puts "Please enter a song name or number:"
   users_input = gets.strip
   
+  # Loop through songs array to find matching song number or song name
   songs.each_with_index do |song_name, idx|
-    song_number = (idx + 1).to_s
+    song_number = (idx + 1).to_s # User's input is a string, must convert song number's to match
     if users_input == song_number || users_input == song_name 
       puts "Playing #{song_name}"
-      return
+      return # Early return so that the "Invalid input" statement won't be output
     end
   end
   puts "Invalid input, please try again"
@@ -30,9 +36,11 @@ def exit_jukebox
 end
 
 def run(songs)
+  # Prompts user for command and store command; .strip strips off white spaces, whereas .chomp takes away new-line character that comes with user's input
   puts "Please enter a command:"
   users_command = gets.strip.chomp
   
+  # Switch case for each of the user's command
   case users_command
   when "help"
     help
